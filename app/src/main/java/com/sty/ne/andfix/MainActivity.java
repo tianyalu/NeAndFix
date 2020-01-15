a1bac087ba06187c19951af32b25dccb9bbf0563
tianyalu <styzf@sina.com>
2020-01-16 00:56:17 +0800

    add storage permission

--- app/src/main/java/com/sty/ne/andfix/MainActivity.java
+++ app/src/main/java/com/sty/ne/andfix/MainActivity.java
@@ -1,9 +1,17 @@
 package com.sty.ne.andfix;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.Environment;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -11,6 +19,7 @@ import android.widget.TextView;
 import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE = 1;
     private Button btnCalculate;
     private Button btnFix;
     private Calculator calculator;
@@ -35,11 +44,57 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 DexManager.getInstance().setContext(MainActivity.this);
-                DexManager.getInstance().loadDex(new File(Environment.getExternalStorageDirectory(),
-                        "/sty/hotfix.dex"));
+                DexManager.getInstance().loadDex(new File(Environment.getExternalStorageDirectory()
+                        + "/sty","hotfix.dex"));
             }
         });
+
+        requestPermission();
     }
 
+    private void requestPermission() {
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED){
+            if(ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE)){
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                        MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE);
+            }else {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                        MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE);
+            }
+        }
+    }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE: {
+                if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    Log.i("sty", "onRequestPermissionResult granted");
+                }else {
+                    Log.i("sty", "onRequestPermissionResult denied");
+                    showWarningDialog();
+                }
+                break;
+            }
+            default:
+                break;
+        }
+    }
+
+    private void showWarningDialog() {
+        AlertDialog dialog = new AlertDialog.Builder(this)
+                .setTitle("警告")
+                .setMessage("请前往设置->应用—>PermissionDemo->权限中打开相关权限，否则功能无法正常使用！")
+                .setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                        //finish();
+                    }
+                }).show();
+    }
 }
